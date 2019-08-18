abd5aa0a391e59cc55df9730eb8b5769da5ac0f1
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-08-18 18:34:53 -0300

    opb as main

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const http = require('https');
 
-var host = 'https://api-2445581589199.staging.gw.apicast.io';
-var paths = ['health', 'server-info', 'hello'];
-var keys = ['04e03a6eb5eefa886b9d8ea526f6e2f2', 
-            '91de66ed27dde17732a45cb94501ef9a', 
-            '9cf24730bedd3ba8b7935d406cd8bd4e', 
-            '92e7b36bd5b49322dd1d6a65eb78e5c4'];
+var host = 'https://open-data-api20-2445581589199.staging.gw.apicast.io';
+var paths = ['banks', 'banks/deutsche-bank-werl', 'banks/deutsche-bank-werl/atms', 'banks/deutsche-bank-werl/products','banks/deutsche-bank-werl/branches'];
+var keys = ['b2fc23d60227388ac387bad214b311a3','15d2ed04a66e3d52b1631ee6ddb0f105','9022657b21d5b7c7f39c20361e2d11fc'];
 
 
 var x = function(_url, _options){
